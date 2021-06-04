b34730ce997ffa18042e18e4a6605b41b825cd0b
xim2016 <xj_ma@yahoo.com>
2021-06-04 16:15:54 -0400

    Removed eppMatrix.c

--- SPaRTAN_python/cythLeastR built/setup.py
+++ SPaRTAN_python/cythLeastR built/setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 from numpy import get_include
 
-ext_main = Extension('cythLeastR', ['cythLeastR/cythLeastR.pyx', 'cythLeastR/ep21R.c', 'cythLeastR/eppMatrix.c', 'cythLeastR/epph.c'], include_dirs=['.', 'cythLeastR', get_include()])
+ext_main = Extension('cythLeastR', ['cythLeastR/cythLeastR.pyx', 'cythLeastR/ep21R.c', 'cythLeastR/epph.c'], include_dirs=['.', 'cythLeastR', get_include()])
 
 setup(ext_modules=cythonize([ext_main]))
